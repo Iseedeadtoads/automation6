4562f4302ae2451efc2a096c7464c35463bcefba
Sereja <sereja.slepov@gmail.com>
2023-07-29 15:59:43 +1000

    initial commit

---
+++ src/test/java/page/TransactionPage.java
@@ -0,0 +1,27 @@
+package page;
+
+import data.DataHelper;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static java.lang.String.valueOf;
+
+public class TransactionPage {
+    private SelenideElement amountField = $("[data-test-id=amount] input");
+    private SelenideElement fromField = $("[data-test-id=from] input");
+    private SelenideElement transferButton = $("[data-test-id=action-transfer]");
+    public void transferMoney(int amount, DataHelper.CardsInfo from) {
+        amountField.setValue(valueOf(amount));
+        fromField.setValue(valueOf(from));
+        transferButton.click();
+    }
+
+    public void errorLimit() {
+        $(".notification__content").should(Condition.exactText("Ошибка"));
+    }
+
+    public void invalidCard() {
+        $(".notification__content").should(Condition.text("Ошибка! Произошла ошибка"));
+    }
+}
